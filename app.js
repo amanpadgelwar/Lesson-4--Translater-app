f16182419bce24867b2cf923f7ebd2799730d172
amanpadgelwar <113186236+amanpadgelwar@users.noreply.github.com>
2022-10-29 15:41:18 +0530

    inout output processing done

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var btnTranslate = document.querySelector("#btn-translate");
 
 btnTranslate.addEventListener("click", clickHandler)
 var outputDiv =document.querySelector(".output1");
+
+
 function clickHandler(){
 outputDiv.innerText ="dasfuhuuscdncb"
 +txtInput.value};
