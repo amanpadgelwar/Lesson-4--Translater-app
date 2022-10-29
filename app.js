44d012d67a3d7a291f4bf2543fb4794ef3e73993
amanpadgelwar <113186236+amanpadgelwar@users.noreply.github.com>
2022-10-29 15:37:35 +0530

    refractor:added style sheet in HTML

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
 
 btnTranslate.addEventListener("click", clickHandler)
+var outputDiv =document.querySelector(".output1");
+function clickHandler(){
+outputDiv.innerText ="dasfuhuuscdncb"
++txtInput.value};
+
+}
