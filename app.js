541dd4d3ffa51d9cfba327f5d658114cd423c430
amanpadgelwar <113186236+amanpadgelwar@users.noreply.github.com>
2022-11-05 21:10:58 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var btn1 =document.querySelector('#btn');
 var inputText =document.querySelector('#inputArea');
+
+var outputText =document.querySelector('#OutputText');
+
 btn1.addEventListener("click",function clickEventHandler(){
-
-console.log("clicked")
-console.log(inputText.value);
-
+outputText.innerHTML ="afkslcnlnlakbkj    "+inputText.value;
 })
 
 
